58473210336625e60862fed2f9ed171f60588e75
Viktor Bubanja <vbu19@uclive.ac.nz>
2019-05-13 18:23:46 +1200

    Make place-holder navigation bar Made a place holder navigation bar just to help with visualising the website. Actual links need to be added later

---
+++ src/components/navigationBar/NavigationBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-toolbar app color="brown darken-4" dark>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>{{ "Review Website" }}</v-toolbar-title>
+    <v-btn flat>Venues</v-btn>
+    <v-spacer></v-spacer>
+    <v-btn flat>SIGN IN</v-btn>
+    <v-btn color="brown lighten-3">JOIN</v-btn>
+  </v-toolbar>
+
+</template>
+
+<script>
+  export default {
+    name: 'AppNavigation'
+  };
+</script>
+
+<style scoped>
+</style>
