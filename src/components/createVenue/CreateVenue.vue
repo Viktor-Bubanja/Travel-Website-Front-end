fbf0c167f3f415e29477263475f2b89ec3397168
Viktor Bubanja <viktor.bubanja@hotmail.com>
2019-05-21 13:57:23 +1200

    Add create venue functionality Users can now create and administer their own venues. A user who uploads a venue automatically becomes the admin.

---
+++ src/components/createVenue/CreateVenue.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-app>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm10 md8 lg6>
+        <v-text-field
+          ref="venueName"
+          v-model="venueName"
+          :rules="rules.venueNameRules"
+          label="Venue name"
+          required
+        ></v-text-field>
+        <v-select
+          ref="categoryId"
+          v-model="categoryId"
+          :rules="rules.categoryRules"
+          label="Category"
+          required
+          item-text="categoryName"
+          item-value="categoryId"
+          :items="categories"
+        ></v-select>
+        <v-text-field
+          ref="shortDescription"
+          v-model="shortDescription"
+          :rules="rules.shortDescriptionRules"
+          label="Short description"
+          required
+        ></v-text-field>
+        <v-text-field
+          ref="longDescription"
+          v-model="longDescription"
+          :rules="rules.longDescriptionRules"
+          label="Long description"
+          required
+        ></v-text-field>
+        <v-text-field
+          ref="city"
+          v-model="city"
+          :rules="rules.cityRules"
+          label="City"
+          required
+        ></v-text-field>
+        <v-text-field
+          ref="address"
+          v-model="address"
+          :rules="rules.addressRules"
+          label="Address"
+          required
+        ></v-text-field>
+        <b-row>
+          <b-col>
+            <v-text-field
+              ref="latitude"
+              v-model="latitude"
+              :rules="rules.latitudeRules"
+              label="Latitude"
+              required
+            ></v-text-field>
+          </b-col>
+          <b-col>
+            <v-text-field
+              ref="longitude"
+              v-model="longitude"
+              :rules="rules.longitudeRules"
+              label="Longitude"
+              required
+            ></v-text-field>
+          </b-col>
+        </b-row>
+        <div align="center">
+          <v-card-actions>
+          <v-btn color="primary" flat @click="submit">Submit</v-btn>
+        </v-card-actions>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-app>
+</template>
+
+<script src="./createVenue.js"></script>
---
+++ src/components/createVenue/createVenue.js
@@ -0,0 +1,90 @@
+export default {
+  data() {
+    return {
+      categories: [],
+      venueName: "",
+      categoryId: "",
+      shortDescription: "",
+      longDescription: "",
+      city: "",
+      address: "",
+      latitude: "",
+      longitude: "",
+      errorMessages: "",
+      rules: {
+        venueNameRules: [(input) => !!input || 'Venue name is required'],
+        categoryRules: [(input) => !!input || 'Category is required'],
+        shortDescriptionRules: [(input) => !!input || 'Short description is required'],
+        longDescriptionRules: [(input) => !!input || 'Long Description is required'],
+        cityRules: [(input) => !!input || 'City is required'],
+        addressRules: [(input) => !!input || 'Address is required'],
+        latitudeRules: [(input) => !!input || 'Latitude is required',
+          (input) => /^\d+$/.test(input) || 'Latitude must be a number'],
+        longitudeRules: [(input) => !!input || 'Longitude is required',
+          (input) => /^\d+$/.test(input) || 'Longitude must be a number']
+      },
+      error: "",
+      errorFlag: false,
+      baseUrl: "http://localhost:4941/api/v1/"
+    }
+  },
+  created() {
+    this.getCategories()
+  },
+  computed: {
+    selectedCategoryId() {
+      return this.category ? this.category['categoryId'] : null
+    },
+    form () {
+      return {
+        venueName: this.venueName,
+        categoryId: this.categoryId,
+        shortDescription: this.shortDescription,
+        longDescription: this.longDescription,
+        city: this.city,
+        address: this.address,
+        latitude: parseFloat(this.latitude),
+        longitude: parseFloat(this.longitude),
+      }
+    }
+  },
+  methods: {
+    getCategories() {
+      this.$http.get(this.baseUrl + 'categories')
+        .then(function (response) {
+          this.categories = response.body;
+        }, function (error) {
+          this.error = error;
+          this.errorFlag = true;
+        });
+    },
+    validateForm() {
+      let formIsValid = true;
+      Object.keys(this.form).forEach(f => {
+        if (this.$refs[f].validate(true) === false) {
+          formIsValid = false;
+        }
+      });
+      return formIsValid;
+    },
+    sendForm(form) {
+      const url = this.baseUrl + 'venues';
+      const headers = {headers: {'X-Authorization': localStorage.getItem("auth")}};
+      return this.$http.post(url, JSON.stringify(this.form), headers)
+    },
+    submit() {
+      this.errorMessages = '';
+      this.formHasErrors = !this.validateForm();
+      if (this.formHasErrors === false) {
+        this.sendForm(this.form)
+          .then(function (response) {
+            console.log(response);
+          }, function (error) {
+            console.log(error);
+            this.errorMessages = "Email or username already in use";
+            this.usernameEmailUnique = false;
+          });
+      }
+    }
+  }
+}
