424c1724195721481d0834f91b92214da566f2ed
Viktor Bubanja <viktor.bubanja@hotmail.com>
2019-05-22 03:18:48 +1200

    Add edit venue functionality Users can now edit the venues they administer.

---
+++ src/components/editVenue/EditVenue.vue
@@ -0,0 +1,5 @@
+<template>
+  <p>sdfgsf</p>
+</template>
+
+<script src="./editVenue.js"></script>
---
+++ src/components/venues/venueDetails/VenueDetails.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <b-card>
+    <div v-if="userIsAdminOfVenue(row.item.admin.userId)" align="right">
+      <b-button v-b-modal.edit-venue-modal>Edit</b-button>
+    </div>
+    <b-modal id="edit-venue-modal" title="Edit Venue">
+      <EditVenue></EditVenue>
+    </b-modal>
+    <h4>About {{ row.item.venueName }}</h4>
+    <div align="right">
+      <p>Category: {{ row.item.category}}</p>
+      <p>Admin: {{ row.item.admin}}</p>
+    </div>
+  </b-card>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        defaultSlotText: "I'll get rendered inside the default slot.",
+      }
+    }
+  }
+</script>
